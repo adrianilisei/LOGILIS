fd93a91cdc199fb9c2ff86560aae26f2eaaba5b3
Adrian Ilisei <adrianilisei@users.noreply.github.com>
2025-10-04 15:22:18 +0300

    replaced logger

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+// middleware.ts
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  console.log(`LOGGER: [${new Date().toISOString()}] ${request.method} ${request.url}`);
+  return NextResponse.next();
+}
+
+export const config = { matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'] };
